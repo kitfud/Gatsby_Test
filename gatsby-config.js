c08affe0efb5dfbe7a6a496c8d86a2fbcb393a13
Kit Fuderich <kitfuderich@gmail.com>
2021-09-07 13:11:55 -0700

    Gatsby tutorial 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "test_website",
+    title: "NFT_website",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
